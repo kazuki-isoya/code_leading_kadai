df744cecbd39888c878b38bd2c7bf2b420fa0826
moru more <moremoru.world21@gmail.com>
2020-03-23 14:55:42 +0900

    定期的にslackにメッセージを送る

--- slack.rb
+++ slack.rb
@@ -1,16 +1,31 @@
 require "slack-notify"
+require "clockwork"
+require 'active_support/time'
 require "dotenv"
 Dotenv.load('.env')
 
-#URLをそのまま書いているのでgithubにpushしないようにする
-client = SlackNotify::Client.new(
-  webhook_url: ENV['WEBHOOK_URL'],
-  channel: "#dive-into-code課題",
-  username: "yuugi_bot",
-  icon_url: "/Users/morumore/Desktop/image/fake_yuugi.jpg",
-  icon_emoji: ":shipit:",
-  link_names: 1
-)
 
+module Clockwork
+  handler do |job|
+    client = SlackNotify::Client.new(
+      webhook_url: ENV['WEBHOOK_URL'],
+      channel: "#dive-into-code課題",
+      username: "yuugi_bot",
+      icon_url: "/Users/morumore/Desktop/image/fake_yuugi.jpg",
+      icon_emoji: ":shipit:",
+      link_names: 1
+    )
+    client.notify("Draw! Monster card!")
+  end
+  every(3.minutes, 'less.frequent.job')
 
-client.notify("my turn end")
+  # client = SlackNotify::Client.new(
+  #   webhook_url: ENV['WEBHOOK_URL'],
+  #   channel: "#dive-into-code課題",
+  #   username: "yuugi_bot",
+  #   icon_url: "/Users/morumore/Desktop/image/fake_yuugi.jpg",
+  #   icon_emoji: ":shipit:",
+  #   link_names: 1
+  # )
+  # client.notify("Draw! Monster card!")
+end
