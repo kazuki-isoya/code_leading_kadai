d2bb5b057a842890925df3c5268122e8752688c9
moru more <moremoru.world21@gmail.com>
2020-03-23 12:46:55 +0900

    dotenvのテスト

--- slack.rb
+++ slack.rb
@@ -1,13 +1,16 @@
 require "slack-notify"
+require "dotenv"
+Dotenv.load('.env')
 
 #URLをそのまま書いているのでgithubにpushしないようにする
 client = SlackNotify::Client.new(
-  webhook_url: "https://hooks.slack.com/services/T010KQ9B9HV/B010KQEUPQF/gbS30ETjXmAGv8DqpL7WsuO",
-  channel: "#dive-into-coke課題",
-  username: "mybot",
+  webhook_url: ENV['WEBHOOK_URL'],
+  channel: "#dive-into-code課題",
+  username: "yuugi_bot",
   icon_url: "/Users/morumore/Desktop/image/fake_yuugi.jpg",
   icon_emoji: ":shipit:",
   link_names: 1
 )
 
-client.notify("kadai test!")
+
+client.notify("my turn end")
